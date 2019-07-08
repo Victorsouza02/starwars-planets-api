f4083e762bc6786c541527cc3f447bc981b64774
Victorsouza02 <victorsouzaleal@gmail.com>
2019-07-08 09:49:08 -0300

    Validação com express-validator

--- src/controllers/planetaController.js
+++ src/controllers/planetaController.js
@@ -1,17 +1,28 @@
 
 const express = require('express');
-
 const request = require('request');
-
+const {check,validationResult} = require('express-validator');
 const Planeta = require('../models/Planeta');
-
 const router = express.Router();
 
 
 // ADICIONA NOVO PLANETA
-router.post('/adicionar', async (req,res) => {
+router.post('/adicionar',[
+    //VALIDAÇÃO DOS CAMPOS
+    check("nome","Campo NOME é obrigatório").isLength({min: 1}),
+    check("clima","Campo CLIMA é obrigatório").isLength({min: 1}),
+    check("terreno","Campo TERRENO é obrigatório").isLength({min: 1})
+], async (req,res) => {
     const { nome } = req.body;
     try {
+        let errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(422).send(
+                { 
+                    errors : errors.array()
+                }
+                );
+        }
 
         if( await Planeta.findOne({ nome }))
             return res.status(400).send({ error: "Planeta já está cadastrado"});
@@ -29,25 +40,6 @@ router.post('/adicionar', async (req,res) => {
         return res.status(400).send({ error : 'Registro falhou'});
     }
 });
-
-
-function pegarAparicoes(nome_filme) {
-    return new Promise((resolve, reject) => {
-        request('https://swapi.co/api/planets/?search='+nome_filme, (error, response, body) => {
-          if (error) {
-            reject(error)
-          } else {
-            try{
-                const info = JSON.parse(body);
-                let films = info['results'][0]['films'];
-                resolve(films.length);
-            } catch (err){
-                resolve(0);
-            }
-          }
-        })
-      })
-}
 
 //LISTAR TODOS OS PLANETAS
 router.get('/', async (req,res) => {
@@ -88,5 +80,24 @@ router.delete('/id/:planetaId',async (req,res) =>{
         return res.status(400).send({ error : 'Erro ao deletar planeta'});
     }
 });
+
+function pegarAparicoes(nome_filme) {
+    return new Promise((resolve, reject) => {
+        request('https://swapi.co/api/planets/?search='+nome_filme, (error, response, body) => {
+          if (error) {
+            reject(error)
+          } else {
+            try{
+                const info = JSON.parse(body);
+                let films = info['results'][0]['films'];
+                resolve(films.length);
+            } catch (err){
+                resolve(0);
+            }
+          }
+        })
+      })
+}
+
 
 module.exports = app => app.use('/planetas', router);
